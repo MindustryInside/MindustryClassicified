a7a8b1c8bc0351e4ba94636ff505620bf1485e09
Félix Córvus <remint32@yandex.ru>
2020-09-23 05:40:07 +0300

    Overriding's scripts

---
+++ scripts/Pal.js
@@ -0,0 +1,92 @@
+// Game colors redefinition
+
+/*
+Pal.items = Color.valueOf("2ea756");
+Pal.command = Color.valueOf("eab678"); //52
+
+Pal.sap = Color.valueOf("665c9f");
+Pal.sapBullet = Color.valueOf("bf92f9");
+Pal.sapBulletBack = Color.valueOf("6d56bf");*/
+
+Pal.spore = Color.valueOf("8a73c6"); //52
+
+Pal.shield = Color.valueOf("f4ba6e"); Pal.shield.a = 0.7; //Pal.shield = Color.valueOf("ffd37f").a(0.7);
+Pal.shieldIn = Color.black.cpy(); Pal.shieldIn.a = 0; //Pal.shieldIn = Color.black.cpy().a(0);
+
+Pal.bulletYellow = Color.valueOf("ffeec9"); //52
+Pal.bulletYellowBack = Color.valueOf("f9c87a"); //52
+
+Pal.darkMetal = Color.valueOf("6e7080");
+Pal.darkerMetal = Color.valueOf("525252"); //52
+
+Pal.missileYellow = Color.valueOf("ffd2ae"); //52
+Pal.missileYellowBack = Color.valueOf("e58956"); //52
+
+Pal.meltdownHit = Color.valueOf("ffb98b");
+
+Pal.plastaniumBack = Color.valueOf("d8d97f"); //52
+Pal.plastaniumFront = Color.valueOf("fffac6"); //52
+
+Pal.lightFlame = Color.valueOf("ffdd55"); //52
+Pal.darkFlame = Color.valueOf("db401c"); //52
+
+Pal.lightPyraFlame = Color.valueOf("ffb855");
+Pal.darkPyraFlame = Color.valueOf("db661c");
+
+Pal.turretHeat = Color.valueOf("ab3400"); //52
+
+Pal.lightOrange = Color.valueOf("f68021"); //52
+Pal.lightishOrange = Color.valueOf("f8ad42"); //52
+Pal.lighterOrange = Color.valueOf("f6e096"); //52
+/*
+Pal.lightishGray = Color.valueOf("a2a2a2"); //52
+Pal.darkishGray = new Color(0.3, 0.3, 0.3, 1); //52
+Pal.darkerGray = new Color(0.2, 0.2, 0.2, 1);
+Pal.darkestGray = new Color(0.1, 0.1, 0.1, 1);
+Pal.ammo = Color.valueOf("ff8947");
+Pal.rubble = Color.valueOf("1c1817");
+
+Pal.boostTo = Color.valueOf("ffad4d");
+Pal.boostFrom = Color.valueOf("ff7f57");
+
+Pal.lancerLaser = Color.valueOf("a9d8ff"); //52
+
+Pal.stoneGray = Color.valueOf("8f8f8f"); //52
+Pal.engine = Color.valueOf("ffbb64");
+
+Pal.health = Color.valueOf("ffff00"); //52
+Pal.heal = Color.valueOf("98ffa9");
+Pal.bar = Color.slate;
+Pal.accent = Color.valueOf("f4ba6e");
+Pal.stat = Color.valueOf("f4ba6e");
+Pal.gray = Color.valueOf("454545");
+Pal.metalGrayDark = Color.valueOf("6e7080");
+Pal.accentBack = Color.valueOf("d4816b");
+Pal.place = Color.valueOf("6335f8"); //52
+Pal.remove = Color.valueOf("e55454"); //52
+Pal.noplace = Color.valueOf("ffa697");
+Pal.removeBack = Color.valueOf("a73e3e");
+Pal.placeRotate = Pal.accent;
+Pal.breakInvalid = Color.valueOf("d44b3d"); //52
+Pal.range = Color.valueOf("f4ba6e"); //52
+Pal.power = Color.valueOf("fbd367"); //52
+Pal.powerBar = Color.valueOf("ec7b4c");
+Pal.powerLight = Color.valueOf("fbd367");
+Pal.placing = Color.valueOf("616161");*/ //52
+
+Pal.unitFront = Color.valueOf("ffa665");
+Pal.unitBack = Color.valueOf("d06b53");
+
+Pal.lightTrail = Color.valueOf("ffe2a9"); //52
+
+Pal.surge = Color.valueOf("b4d5c7"); //52
+
+Pal.plastanium = Color.valueOf("a1b46e");
+
+Pal.redSpark = Color.valueOf("fbb97f"); //52
+Pal.orangeSpark = Color.valueOf("d2b29c"); //52
+
+Pal.redDust = Color.valueOf("ffa480"); //52
+Pal.redderDust = Color.valueOf("ff7b69"); //52
+
+Pal.plasticSmoke = Color.valueOf("f1e479"); //52
